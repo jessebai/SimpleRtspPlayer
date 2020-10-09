2898d0fdec2df13b028a6574865dd51f4f9a06b2
柏正权 <632104384@qq.com>
2020-10-09 15:12:19 +0800

    move all the func to audioView.xaml.cs first, enable the button

--- SimpleRtspPlayer/GUI/Views/AudioView.xaml.cs
+++ SimpleRtspPlayer/GUI/Views/AudioView.xaml.cs
@@ -15,6 +15,13 @@ using System.Windows.Forms.DataVisualization.Charting;
 using NAudio.Wave;
 using NAudio.Wave.SampleProviders;
 using System.Collections;
+using GalaSoft.MvvmLight.Command;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.CompilerServices;
+using RtspClientSharp;
+using SimpleRtspPlayer.GUI.Models;
+using SimpleRtspPlayer.RawFramesReceiving;
 
 namespace SimpleRtspPlayer.GUI.Views
 {
@@ -25,7 +32,7 @@ namespace SimpleRtspPlayer.GUI.Views
     {
         private readonly Action<IDecodedAudioFrame> _invalidateAction;
         private ArrayList audioSeriesData  = new ArrayList();
-        private PCMPlayer audioPlayer = new PCMPlayer();
+        public PCMPlayer audioPlayer = new PCMPlayer();
 
         public static readonly DependencyProperty AudioSourceProperty = DependencyProperty.Register(nameof(AudioSource),
             typeof(IAudioSource),
@@ -37,12 +44,68 @@ namespace SimpleRtspPlayer.GUI.Views
             set => SetValue(AudioSourceProperty, value);
         }
 
+        //private bool _startButtonEnabled = true;
+        //private bool _stopButtonEnabled;
+        //private bool _silentButtonEnabled;
+        private const string RtspPrefix = "rtsp://";
+        private const string HttpPrefix = "http://";
+        private string _status = string.Empty;
+        private readonly RealtimeVideoSource _realtimeVideoSource = new RealtimeVideoSource();
+        private readonly RealtimeAudioSource _realtimeAudioSource = new RealtimeAudioSource();
+        private IRawFramesSource _rawFramesSource;
+        public event EventHandler<string> StatusChanged;
+        public IVideoSource I_VideoSource => _realtimeVideoSource;
+        public IAudioSource I_AudioSource => _realtimeAudioSource;
+        public string DeviceAddress { get; set; } = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
+        public string Login { get; set; } = "";
+        public string Password { get; set; } = "";
+        public RelayCommand AudioStartClickCommand { get; }
+        public RelayCommand AudioStopClickCommand { get; }
+        public RelayCommand AudioSilentClickCommand { get; }
+        private void OnAudioStartClick(object sender, RoutedEventArgs e)
+        {
+            string address = DeviceAddress;
+
+            if (!address.StartsWith(RtspPrefix) && !address.StartsWith(HttpPrefix))
+                address = RtspPrefix + address;
+
+            if (!Uri.TryCreate(address, UriKind.Absolute, out Uri deviceUri))
+            {
+                System.Windows.MessageBox.Show("Invalid device address", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var credential = new NetworkCredential(Login, Password);
+            var connectionParameters = new ConnectionParameters(deviceUri, credential);
+
+
+            if (_rawFramesSource != null)
+                return;
+
+            _rawFramesSource = new RawFramesSource(connectionParameters);
+            //_rawFramesSource.ConnectionStatusChanged += ConnectionStatusChanged;
+
+            _realtimeVideoSource.SetRawFramesSource(_rawFramesSource);
+            _realtimeAudioSource.SetRawFramesSource(_rawFramesSource);
+
+            _rawFramesSource.Start();
+
+            //_mainWindowModel.Start(connectionParameters);
+            //_mainWindowModel.StatusChanged += MainWindowModelOnStatusChanged;
+        }
+        private void OnAudioStopClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void OnAudioSilentClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         public AudioView()
         {
             InitializeComponent();
             _invalidateAction = Invalidate;
             ChartAudio1.Titles.Add("音频1");
-            //Play(audioProvider);
         }
 
         private static void OnAudioSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -125,6 +188,7 @@ namespace SimpleRtspPlayer.GUI.Views
         {
             audioPlayer.CancelSilentPlay();
         }
+
     }
 
     public class PCMPlayer
