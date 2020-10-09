3ef6a4b0ed1e56a877d562161fd34dff470a0e0c
柏正权 <632104384@qq.com>
2020-10-09 16:05:54 +0800

    show audio signal successfully

--- SimpleRtspPlayer/GUI/Views/AudioView.xaml.cs
+++ SimpleRtspPlayer/GUI/Views/AudioView.xaml.cs
@@ -22,6 +22,16 @@ using System.Runtime.CompilerServices;
 using RtspClientSharp;
 using SimpleRtspPlayer.GUI.Models;
 using SimpleRtspPlayer.RawFramesReceiving;
+using System.Security.Authentication;
+using RtspClientSharp.RawFrames;
+using RtspClientSharp.Rtsp;
+using System.Collections.Generic;
+using RtspClientSharp.RawFrames;
+using RtspClientSharp.RawFrames.Audio;
+using SimpleRtspPlayer.RawFramesDecoding;
+using SimpleRtspPlayer.RawFramesDecoding.DecodedFrames;
+using SimpleRtspPlayer.RawFramesDecoding.FFmpeg;
+using SimpleRtspPlayer.RawFramesReceiving;
 
 namespace SimpleRtspPlayer.GUI.Views
 {
@@ -53,15 +63,18 @@ namespace SimpleRtspPlayer.GUI.Views
         private readonly RealtimeVideoSource _realtimeVideoSource = new RealtimeVideoSource();
         private readonly RealtimeAudioSource _realtimeAudioSource = new RealtimeAudioSource();
         private IRawFramesSource _rawFramesSource;
-        public event EventHandler<string> StatusChanged;
+        //public event EventHandler<string> StatusChanged;
         public IVideoSource I_VideoSource => _realtimeVideoSource;
         public IAudioSource I_AudioSource => _realtimeAudioSource;
         public string DeviceAddress { get; set; } = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
         public string Login { get; set; } = "";
         public string Password { get; set; } = "";
-        public RelayCommand AudioStartClickCommand { get; }
-        public RelayCommand AudioStopClickCommand { get; }
-        public RelayCommand AudioSilentClickCommand { get; }
+        private Task _workTask = Task.CompletedTask;
+        private CancellationTokenSource _cancellationTokenSource;
+        private static readonly TimeSpan RetryDelay = TimeSpan.FromSeconds(5);
+        private ConnectionParameters _connectionParameters;
+        private readonly Dictionary<FFmpegAudioCodecId, FFmpegAudioDecoder> _audioDecodersMap =
+            new Dictionary<FFmpegAudioCodecId, FFmpegAudioDecoder>();
         private void OnAudioStartClick(object sender, RoutedEventArgs e)
         {
             string address = DeviceAddress;
@@ -76,18 +89,27 @@ namespace SimpleRtspPlayer.GUI.Views
             }
             var credential = new NetworkCredential(Login, Password);
             var connectionParameters = new ConnectionParameters(deviceUri, credential);
-
+            _connectionParameters = connectionParameters;
 
             if (_rawFramesSource != null)
                 return;
 
-            _rawFramesSource = new RawFramesSource(connectionParameters);
-            //_rawFramesSource.ConnectionStatusChanged += ConnectionStatusChanged;
+            //_rawFramesSource = new RawFramesSource(connectionParameters);
+            ////_rawFramesSource.ConnectionStatusChanged += ConnectionStatusChanged;
 
-            _realtimeVideoSource.SetRawFramesSource(_rawFramesSource);
-            _realtimeAudioSource.SetRawFramesSource(_rawFramesSource);
+            //_realtimeVideoSource.SetRawFramesSource(_rawFramesSource);
+            //_realtimeAudioSource.SetRawFramesSource(_rawFramesSource);
+            //_rawFramesSource.Start();
 
-            _rawFramesSource.Start();
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            CancellationToken token = _cancellationTokenSource.Token;
+
+            _workTask = _workTask.ContinueWith(async p =>
+            {
+                await ReceiveAsync(token);
+            }, token);
+
 
             //_mainWindowModel.Start(connectionParameters);
             //_mainWindowModel.StatusChanged += MainWindowModelOnStatusChanged;
@@ -99,6 +121,98 @@ namespace SimpleRtspPlayer.GUI.Views
         private void OnAudioSilentClick(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private async Task ReceiveAsync(CancellationToken token)
+        {
+            try
+            {
+                using (var rtspClient = new RtspClient(_connectionParameters))
+                {
+                    rtspClient.FrameReceived += RtspClientOnFrameReceived;
+
+                    while (true)
+                    {
+                        //OnStatusChanged("Connecting...");
+
+                        try
+                        {
+                            await rtspClient.ConnectAsync(token);
+                        }
+                        catch (InvalidCredentialException)
+                        {
+                            //OnStatusChanged("Invalid login and/or password");
+                            await Task.Delay(RetryDelay, token);
+                            continue;
+                        }
+                        catch (RtspClientException e)
+                        {
+                            //OnStatusChanged(e.ToString());
+                            await Task.Delay(RetryDelay, token);
+                            continue;
+                        }
+
+                        //OnStatusChanged("Receiving frames...");
+
+                        try
+                        {
+                            await rtspClient.ReceiveAsync(token);
+                        }
+                        catch (RtspClientException e)
+                        {
+                            //OnStatusChanged(e.ToString());
+                            await Task.Delay(RetryDelay, token);
+                        }
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+        }
+
+        private void RtspClientOnFrameReceived(object sender, RawFrame rawFrame)
+        {
+            if (!(rawFrame is RawAudioFrame rawAudioFrame))
+                return;
+
+            FFmpegAudioDecoder decoder = GetDecoderForFrame(rawAudioFrame);
+
+            if (!decoder.TryDecode(rawAudioFrame))
+                return;
+
+            IDecodedAudioFrame decodedFrame = decoder.GetDecodedFrame(new AudioConversionParameters() { OutBitsPerSample = 16 });
+            System.Windows.Application.Current.Dispatcher.Invoke(_invalidateAction, DispatcherPriority.Send, decodedFrame);
+        }
+        private FFmpegAudioDecoder GetDecoderForFrame(RawAudioFrame audioFrame)
+        {
+            FFmpegAudioCodecId codecId = DetectCodecId(audioFrame);
+
+            if (!_audioDecodersMap.TryGetValue(codecId, out FFmpegAudioDecoder decoder))
+            {
+                int bitsPerCodedSample = 0;
+
+                if (audioFrame is RawG726Frame g726Frame)
+                    bitsPerCodedSample = g726Frame.BitsPerCodedSample;
+
+                decoder = FFmpegAudioDecoder.CreateDecoder(codecId, bitsPerCodedSample);
+                _audioDecodersMap.Add(codecId, decoder);
+            }
+
+            return decoder;
+        }
+        private FFmpegAudioCodecId DetectCodecId(RawAudioFrame audioFrame)
+        {
+            if (audioFrame is RawAACFrame)
+                return FFmpegAudioCodecId.AAC;
+            if (audioFrame is RawG711AFrame)
+                return FFmpegAudioCodecId.G711A;
+            if (audioFrame is RawG711UFrame)
+                return FFmpegAudioCodecId.G711U;
+            if (audioFrame is RawG726Frame)
+                return FFmpegAudioCodecId.G726;
+
+            throw new ArgumentOutOfRangeException(nameof(audioFrame));
         }
 
         public AudioView()
